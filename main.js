5ebd25c75d50c749eaaf95d270d2a55e1549b87a
Reuben <reubenthomas99@gmail.com>
2023-07-29 15:00:22 +0530

    Add functionality and styles

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 let myLibrary = [
-    {
-        title: 'The Alchemist',
-        author: 'Paulo Coelho',
-        noOfPages: 150,
-        isRead: true
-    },
-    {
-        title: 'Deep Work',
-        author: 'Cal Newport',
-        noOfPages: 200,
-        isRead: true
-    },
-    {
-        title: 'The Alchemist',
-        author: 'Paulo Coelho',
-        noOfPages: 150,
-        isRead: false
-    }
+    // {
+    //     title: 'The Alchemist',
+    //     author: 'Paulo Coelho',
+    //     noOfPages: 150,
+    //     isRead: true
+    // },
+    // {
+    //     title: 'Deep Work',
+    //     author: 'Cal Newport',
+    //     noOfPages: 200,
+    //     isRead: true
+    // },
+    // {
+    //     title: 'The Alchemist',
+    //     author: 'Paulo Coelho',
+    //     noOfPages: 150,
+    //     isRead: false
+    // }
 ];
 
 function Book(title, author, noOfPages, isRead) {
@@ -24,38 +24,86 @@ function Book(title, author, noOfPages, isRead) {
     this.author = author;
     this.noOfPages = noOfPages;
     this.isRead = isRead;
-    this.showInfo = function() {
-        return `${title} by ${author}, ${noOfPages} pages, ${isRead ? 'has read' : 'not read yet'}`;
-    }
 }
 
-function addBookToLibrary() {
-    const titleInput = document.getElementById('titleInput').value;
+Book.prototype.showInfo = function() {
+    return `${this.title} by ${this.author}, ${this.noOfPages} pages, ${this.isRead ? 'Completed Reading' : 'Not read yet'}`;
+}
+
+Book.prototype.toggleRead = function() {
+    return this.isRead = !this.isRead;
+    
+}
+
+function addBookToLibrary(e) {
+    let titleInput = document.getElementById('titleInput').value;
     const authorInput = document.getElementById('authorInput').value;
     const pagesInput = document.getElementById('pagesInput').value;
-    const readInput = document.querySelector('input[name="isRead"]:checked').value;
-    console.log(readInput);
-    // const newt = new Book(titleInput, authorInput, pagesInput);
+    // let readInput = document.querySelector('input[name="isRead"]:checked').value;
+    const readInput = document.querySelector('input[type="checkbox"]').checked;
+    const form = document.querySelector('form');
+    if (titleInput && authorInput) {
+        e.preventDefault();
+        console.log(e);
+        myLibrary.push(new Book(titleInput, authorInput, pagesInput, readInput));
+        titleInput = null;
+        form.reset();
+        createForm.classList.toggle('seen');
+        cards.textContent = '';
+        displayBooks();
+        
+    }   
+    
+    // console.info(myLibrary);
+    
 }
 
 const main = document.querySelector('main');
+const cards = document.querySelector('#cards');
+
 
 
 function displayBooks() {
-    myLibrary.forEach((book) => {
+    myLibrary.forEach((book, index) => {
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
-        const title = document.createElement('h4');
-        const author = document.createElement('h6');
+        const title = document.createElement('h3');
+        const author = document.createElement('h5');
         const noOfPages = document.createElement('p');
+        const readStatus = document.createElement('p');
+        const buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+        const readToggleBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');
+        readToggleBtn.textContent = (book.isRead) ? 'Mark not Read' : 'Mark as Read';
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.setAttribute('data-id', index);
         title.textContent = book.title;
         author.textContent = book.author;
-        noOfPages.textContent = book.noOfPages;
-        card.append(title, author, noOfPages);
-        main.appendChild(card);
+        noOfPages.textContent = 'Pages: '+ book.noOfPages;
+        readStatus.textContent = (book.isRead) ? 'Completed Reading' : "Not Read";
+        buttons.append(readToggleBtn, deleteBtn);
+        card.append(title, author, noOfPages, readStatus, buttons);
+        cards.appendChild(card);
+
+        deleteBtn.addEventListener('click', (e) => {
+            const itemForDelete = e.target.dataset.id;
+            myLibrary.splice(itemForDelete, 1);
+            cards.textContent = '';
+            displayBooks();
+        });
+        
+        readToggleBtn.addEventListener('click', () => {
+            book.toggleRead();
+            cards.textContent = '';
+            displayBooks();
+        })
     })
 }
 
+
+
+//Display on page load
 displayBooks();
 
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', false);
@@ -66,8 +114,19 @@ const addBtn = document.querySelector('.add-btn');
 const createForm = document.querySelector('.form');
 const createBtn = document.querySelector('.create-btn')
 
-addBtn.addEventListener('click', () => {
+
+addBtn.addEventListener('click', (e) => {       
+    e.stopPropagation(); 
     createForm.classList.toggle('seen');
 })
 
+document.addEventListener('click', (e) => {
+    if(!e.target.matches('form') && !e.target.matches('input') && !e.target.matches('button') && !e.target.matches('label') && !e.target.matches('div')) {
+        if(createForm.classList.contains('seen')) {
+            createForm.classList.remove('seen');
+        }
+    }
+})
+
 createBtn.addEventListener('click', addBookToLibrary);
+
